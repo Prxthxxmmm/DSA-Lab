15215f9c66014b8114fbe7d5ee1f723bcc323baf
Pratham <91910477+Prxthxxmmm@users.noreply.github.com>
2022-07-27 23:06:28 +0530

    Create program-01.c

---
+++ Assignment-1/01_Bitwise-Operators/program-01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter any number:");
+    scanf("%d", &n);
+
+    if(n & 1)
+    {
+        printf("%d is odd", n);
+    }
+    else
+    {
+        printf("%d is even", n);
+    }
+    return 0;
+}
